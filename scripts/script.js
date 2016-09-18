99590e3121b7997b1ee82682b9c4564d1f5a9b13
7buck7 <owencallen@gmail.com>
2016-09-18 17:54:25 -0400

    renamed script to quote for increased clarity

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-//this is all for the quote generator
-var qButton = document.querySelector("#quoteButton");
-var qText = document.querySelector("#quote");
-
-qButton.addEventListener("click", function(event) {
-  event.target.innerHTML = "<b>You Click Me!</b>";
-});
