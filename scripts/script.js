05ef585822c773860ab0afc5db0fbdda623a65c8
7buck7 <owencallen@gmail.com>
2016-09-18 14:21:23 -0400

    button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
 var javascript = lol;
+var qButton = document.querySelector("#quoteButton");
+qButton.addEventListener("click", function(event) {
+  event.target.innerHTML = "<b>You Click Me!</b>";
+});
