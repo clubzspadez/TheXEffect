24d66fd64e8fdabd4fb82c73e5c624acd074c7bc
chakrm <mishachakrabarti@gmail.com>
2016-09-18 14:35:25 -0400

    Added style changes "#   (use ""git push"" to publish your local commits)" "#	modified:   scripts/script.js"

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,1 @@
 var javascript = lol;
-var qButton = document.querySelector("#quoteButton");
-var qText = document.querySelector("#quote");
-
-qButton.addEventListener("click", function(event) {
-  qText.innerHTML = "insperational BS!";
-});
