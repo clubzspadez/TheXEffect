68aecd5403fce739245d8b98122521740775a079
7buck7 <owencallen@gmail.com>
2016-09-18 18:06:32 -0400

    cleaning up css

---
+++ scripts/quote.js
@@ -0,0 +1,7 @@
+//this is all for the quote generator
+var qButton = document.querySelector("#quoteButton");
+var qText = document.querySelector("#quote");
+
+qButton.addEventListener("click", function(event) {
+  qText.innerHTML = "inspirational BS!";
+});
