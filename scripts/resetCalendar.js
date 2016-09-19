5c097bf0459faafa95f8b63c9a585f9e54ef1db4
7buck7 <owencallen@gmail.com>
2016-09-19 18:21:07 -0400

    fixed the reset button

--- scripts/resetCalendar.js
+++ scripts/resetCalendar.js
@@ -1,1 +1,8 @@
-alert("fix resetCalendar button");
+var resetB = document.querySelector("#resetCalendar");
+
+resetB.addEventListener("click", function (event) {
+  var dotm = document.querySelectorAll(".dayOfTheMonth");
+  for (var i = 0; i < dotm.length; i++) {
+    dotm[i].style.backgroundColor = "grey";
+  }
+});
