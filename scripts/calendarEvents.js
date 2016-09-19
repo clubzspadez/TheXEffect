13961d87b8bc06a7a2e97ebe506e2ea832fd2b71
7buck7 <owencallen@gmail.com>
2016-09-19 18:06:10 -0400

    days now change color on click

--- scripts/calendarEvents.js
+++ scripts/calendarEvents.js
@@ -1,9 +1,10 @@
-alert("this works");
 var cal = document.querySelector("#calendar");
 cal.addEventListener("click", function (event){
-//  if (event.target.className == "dayOfTheMonth"){
-    event.target.style.backGroundColor = black;
-//  }
+  if (event.target.className == "dayOfTheMonth"){
+    if(event.target.style.backgroundColor !== "black"){
+      event.target.style.backgroundColor = "black";
+    }else {
+      event.target.style.backgroundColor = "grey";
+    }
+  }
 });
-
-function findDay ()
