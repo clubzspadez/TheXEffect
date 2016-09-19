d7dcb3e1f43bc0c63beae77114b5b3ad21f3063d
7buck7 <owencallen@gmail.com>
2016-09-19 17:53:02 -0400

    adding interactivity for days of the month

---
+++ scripts/calendarEvents.js
@@ -0,0 +1,9 @@
+alert("this works");
+var cal = document.querySelector("#calendar");
+cal.addEventListener("click", function (event){
+//  if (event.target.className == "dayOfTheMonth"){
+    event.target.style.backGroundColor = black;
+//  }
+});
+
+function findDay ()
